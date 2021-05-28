d2fc0daafc10acd09bbce94c483a3518c6f85968
Frank Michael Arildsen <f.arildsen@gmail.com>
2021-05-28 19:09:37 +0200

    Prettier format

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 
-function myFunc() {
-      console.log("klik")
-} 
+function toggleBtn(e) {
+      const htmlEl = document.documentElement
+      const el = document.getElementById("checkmark")
+      let targetEl = document.getElementById('theme-' + e)
+      htmlEl.setAttribute('data-theme', 'theme-' + e)
+      targetEl.appendChild(el)
+    }
 
 
